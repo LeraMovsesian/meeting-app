730edfa5235cd1cd0600d908296263134eaaf7ee
Lera <lera.24054@gmail.com>
2024-05-01 23:27:38 +0600

    implemented stream videocall sdk

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,15 +1,51 @@
 'use client';
-import Image from 'next/image';
 import React, {useState} from "react";
 import HomeCard from "@/components/HomeCard";
 import {useRouter} from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
+import {useUser} from "@clerk/nextjs";
+import {Call, useStreamVideoClient} from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
+    const { user } = useUser();
+    const client = useStreamVideoClient();
+    const [values, setValues] = useState({
+        dateTime: new Date(),
+        link: '',
+        description: '',
+    });
+    const [callDetails, setCallDetails] = useState<Call>();
 
-    const createMeeting = () => {};
+    const createMeeting = async () => {
+        if (!client || !user) return;
+
+        try {
+            const id = crypto.randomUUID();
+            const call = client.call('default', id);
+
+            if (!call) throw new Error('Failed to create a call');
+
+            const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+            const description = values.description || 'Instant meeting';
+            await call.getOrCreate({
+                data: {
+                    starts_at: startsAt,
+                    custom: {
+                        description,
+                    },
+                }
+            });
+            setCallDetails(call);
+
+            if (!values.description) {
+                router.push(`/meeting/${call.id}`)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    };
 
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -18,7 +54,7 @@ const MeetingTypeList = () => {
                 img='/icons/add-meeting.svg'
                 title='New meeting'
                 description='Start an instant meeting'
-                handleClick={() => setMeetingState('isJoiningMeeting')}
+                handleClick={() => setMeetingState('isInstantMeeting')}
             />
             <HomeCard
                 className='bg-blue-1'
@@ -43,7 +79,7 @@ const MeetingTypeList = () => {
             />
             <MeetingModal
                 isOpen={meetingState === 'isInstantMeeting'}
-                onClosr={() => setMeetingState(undefined)}
+                onClose={() => setMeetingState(undefined)}
                 title='Start an instant meeting'
                 className='text-center'
                 buttonText='Start meeting'
