dd1b5d6c090fbd068e0ea42f3d8ec092c985f81f
Lera <lera.24054@gmail.com>
2024-05-07 14:49:03 +0600

    completed creating instant meeting functional

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,14 @@
 import React, {ReactNode} from 'react';
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import type {Metadata} from "next";
 
+export const metadata: Metadata = {
+    title: "YOOM",
+    description: "Modern video calling app",
+    icons: {
+        icon: '/icons/logo.svg',
+    }
+};
 const RootLayout = ({ children }: {children: ReactNode}) => {
     return (
         <main>
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import {VideoPreview} from "@stream-io/video-react-sdk";
+'use client';
+import React, {useEffect, useState} from 'react';
+import {DeviceSettings, useCall, VideoPreview} from "@stream-io/video-react-sdk";
+import {Button} from "@/components/ui/button";
 
-const MeetingSetup = () => {
+const MeetingSetup = ({ setIsSetupComplete }: { setIsSetupComplete: (value: boolean) => void }) => {
+    const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false);
+    const call = useCall();
+
+    if (!call) {
+        throw new Error('Call is not available');
+    }
+
+    useEffect(() => {
+        if (isMicCamToggleOn) {
+            call?.camera.disable();
+            call?.microphone.disable();
+        } else {
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+    }, [isMicCamToggleOn, call?.camera, call?.microphone]);
     return (
         <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
             <h1 className='text-2xl font-bold'>Setup</h1>
-            <VideoPreview/>
+            <VideoPreview />
+            <div className='flex h-16 items-center justify-center gap-3'>
+                <label className='flex items-center justify-center gap-2 font-medium'>
+                    <input
+                        type="checkbox"
+                        checked={isMicCamToggleOn}
+                        onChange={(e) => setIsMicCamToggleOn(e.target.checked)}
+                    />
+                    Join with mic and camera off
+                </label>
+                <DeviceSettings />
+            </div>
+            <Button
+                className='rounded-md bg-green-500 px-4 py-2.5'
+                onClick={() => {
+                    call.join();
+                    setIsSetupComplete(true)
+                }}
+            >
+                Join meeting
+            </Button>
         </div>
     );
 };
